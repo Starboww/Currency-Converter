a60c0a8219f8337e6afed162f52cec7781606fd3
Suraj Prasad Soni <Surajsoni119@gmail.com>
2020-06-08 21:59:45 +0530

    icon added

---
+++ app/src/main/java/in/starbow/currencyconverter/MainActivity.java
@@ -0,0 +1,235 @@
+package in.starbow.currencyconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+import java.util.Formatter;
+
+public class MainActivity extends AppCompatActivity {
+Button euro,pound,dollar,yen,dinar,bitcoin,rubel,aus_dollar,can_dollar;
+EditText editText;
+TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        euro =findViewById(R.id.euro);
+        pound =findViewById(R.id.pound);
+        dollar = findViewById(R.id.dollar);
+        yen = findViewById(R.id.yen);
+        dinar =findViewById(R.id.dinar);
+        bitcoin= findViewById(R.id.bitcoin);
+         rubel= findViewById(R.id.rubel);
+        aus_dollar = findViewById(R.id.austerallian_dollar);
+        can_dollar = findViewById(R.id.canadian_dollar);
+        editText =findViewById(R.id.editText);
+        textView=findViewById(R.id.textView);
+        /////////////
+        euro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            String z =editText.getText().toString();
+            // to check whether the user is submitting empty form or not
+                if(TextUtils.isEmpty(z))
+                {
+                    editText.setError("Enter Something");
+                }
+                else{
+                    double n,k;
+                    n= Double.parseDouble(z);//converting string back to int
+//                    editText.setText(null);
+                   // Formatter formatter = new Formatter();//by this class we can format our class in the we we want
+                    k=n*0.012;
+                    //way 1
+                    DecimalFormat numberFormat = new DecimalFormat("#.000");
+                    textView.setText(""+numberFormat.format(k));
+
+                    //way 2
+                    //textView.setText(""+k);//as text view only accepts string so we have to concatenate number(k) with "" to make it a string
+                                             //this is a easier way to do the above part but its not the ideal way as we cant set the
+                                             //number of values after decimal
+                }
+            }
+        });
+
+        pound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String z =editText.getText().toString();
+                if(TextUtils.isEmpty(z))
+                {
+                    editText.setError("Enter Something");
+                }
+                else {
+                    double n, k;
+                    n = Double.parseDouble(z);
+                    textView.setText(null);
+                    k = n * 0.0104;
+                    DecimalFormat numberFormat = new DecimalFormat("#.000");
+                    textView.setText("" + numberFormat.format(k));
+                }
+
+
+                }
+        });
+
+
+        dollar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String z =editText.getText().toString();
+                if(TextUtils.isEmpty(z))
+                {
+                    editText.setError("Enter Something");
+                }
+                else {
+                    double n, k;
+                    n = Double.parseDouble(z);
+                    textView.setText(null);
+                    k = n * 0.0132;
+                    DecimalFormat numberFormat = new DecimalFormat("#.00");
+                    textView.setText("" + numberFormat.format(k));
+                }
+
+            }
+        });
+
+
+        yen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String z =editText.getText().toString();
+                if(TextUtils.isEmpty(z))
+                {
+                    editText.setError("Enter Something");
+                }
+                else {
+                    double n, k;
+                    n = Double.parseDouble(z);
+                    textView.setText(null);
+                    k = n * 1.449;
+                    DecimalFormat numberFormat = new DecimalFormat("#.000");
+                    textView.setText("" + numberFormat.format(k));
+                }
+
+            }
+        });
+
+        dinar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String z =editText.getText().toString();
+                if(TextUtils.isEmpty(z))
+                {
+                    editText.setError("Enter Something");
+                }
+                else {
+                    double n, k;
+                    n = Double.parseDouble(z);
+                    textView.setText(null);
+                    k = n * 0.0040;
+                    DecimalFormat numberFormat = new DecimalFormat("#.000");
+                    textView.setText("" + numberFormat.format(k));
+                }
+
+            }
+        });
+
+
+        bitcoin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String z =editText.getText().toString();
+                if(TextUtils.isEmpty(z))
+                {
+                    editText.setError("Enter Something");
+                }
+                else {
+                    double n, k;
+                    n = Double.parseDouble(z);
+                    textView.setText(null);
+                    k = n * 0.00000136;
+                    DecimalFormat numberFormat = new DecimalFormat("#.00000");
+                    textView.setText("" + numberFormat.format(k));
+                }
+
+            }
+        });
+
+
+
+        rubel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String z =editText.getText().toString();
+                if(TextUtils.isEmpty(z))
+                {
+                    editText.setError("Enter Something");
+                }
+                else {
+                    double n, k;
+                    n = Double.parseDouble(z);
+                    textView.setText(null);
+                    k = n * 0.9009;
+                    DecimalFormat numberFormat = new DecimalFormat("#.000");
+                    textView.setText("" + numberFormat.format(k));
+                }
+
+            }
+        });
+
+
+
+        aus_dollar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String z =editText.getText().toString();
+                if(TextUtils.isEmpty(z))
+                {
+                    editText.setError("Enter Something");
+                }
+                else {
+                    double n, k;
+                    n = Double.parseDouble(z);
+                    textView.setText(null);
+                    k = n * 0.0189;
+                    DecimalFormat numberFormat = new DecimalFormat("#.000");
+                    textView.setText("" + numberFormat.format(k));
+                }
+
+            }
+        });
+
+
+
+        can_dollar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String z =editText.getText().toString();
+                if(TextUtils.isEmpty(z))
+                {
+                    editText.setError("Enter Something");
+                }
+                else {
+                    double n, k;
+                    n = Double.parseDouble(z);
+                    textView.setText(null);
+                    k = n *0.0177 ;
+
+                    DecimalFormat numberFormat = new DecimalFormat("#.000");
+                    textView.setText("" + numberFormat.format(k));
+                }
+
+            }
+        });
+
+    }
+}
